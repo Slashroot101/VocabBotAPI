70ae2ffe5c32a3683144dd0bf6e29680eb601231
currysl <currysl@ucmail.uc.edu>
2018-07-23 15:57:16 -0400

    lesson schema

---
+++ src/libs/models/Lesson.js
@@ -0,0 +1,58 @@
+const mongoose = require(`mongoose`);
+const Schema = mongoose.Schema;
+let moment = require(`moment`);
+
+let Lesson = new Schema({
+  siteLessonID: {
+    type: String,
+    required: true,
+    index: {
+      unique: true
+    }
+  },
+  totalQuestionsAnswered: {
+    type: Number,
+    required: true,
+    index: {
+      unique: false
+    }
+  },
+  totalQuestionsAnsweredCorrectly: {
+    type: Number,
+    required: true,
+    index: {
+      unique: false
+    }
+  },
+  users: [{
+    id: {
+      type:  mongoose.Schema.Types.ObjectId,
+      ref: `User`
+    },
+    totalQuestionsAnswered: {
+      type: Number,
+      required: True,
+      index: {
+        unique: false
+      }
+    },
+    totalQuestionsAnsweredCorrectly: {
+      type: Number,
+      required: true,
+      index: {
+        unique: false
+      }
+    },
+    startDate: {
+      type: Date,
+      required: true,
+      index: {
+        unique: false
+      },
+      default: moment()
+    }
+  }]
+});
+
+module.exports = mongoose.model('Lesson', Lesson);
+
