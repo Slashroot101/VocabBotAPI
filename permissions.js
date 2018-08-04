e3977300d1f5f2ff649e89c9567c3b00340fc2f5
Samuel Curry <samc735@gmail.com>
2018-08-04 00:23:48 -0400

    added some permission constants

---
+++ permissions.js
@@ -0,0 +1,26 @@
+module.exports = {
+    lesson: {
+        READ: `CAN_READ_LESSON`,
+        WRITE: `CAN_WRITE_LESSON`,
+        EDIT: `CAN_UPDATE_LESSON`,
+        DELETE: `CAN_DELETE_LESSON`
+    },
+    permission: {
+        READ: `CAN_READ_PERMISSIONS`,
+        WRITE: `CAN_WRITE_PERMISSIONS`,
+        EDIT: `CAN_UPDATE_PERMISSIONS`,
+        DELETE: `CAN_DELETE_PERMISSIONS`
+    },
+    role: {
+        READ: `CAN_READ_ROLE`,
+        WRITE: `CAN_WRITE_ROLE`,
+        EDIT: `CAN_UPDATE_ROLE`,
+        DELETE: `CAN_DELETE_ROLE`
+    },
+    user: {
+        READ: `CAN_READ_USER`,
+        WRITE: `CAN_WRITE_USER`,
+        EDIT: `CAN_UPDATE_USER`,
+        DELETE: `CAN_DELETE_USER`
+    }
+};
